e2cd259f8ab698841906f67cfa9859ea33b84948
esseppidefi <97457479+esseppidefi@users.noreply.github.com>
2022-02-24 17:35:04 +0100

    mail completata

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-const accounts = ["Ciao@gmail.com", "william@gmail.com", "yoyo@gmail.com", "basta@gmail.com", "bella@gmail.com"]
-let main = document.querySelector('main')
-let div = document.createElement('div')
+const accounts = ["ciao@gmail.com", "william@gmail.com", "yoyo@gmail.com", "basta@gmail.com", "bella@gmail.com"]
+let email = document.getElementById('email');
+let main = document.querySelector('main');
+let div = document.createElement('div');
+let button = document.querySelector('button');
 
-// const email = document.getElementById(email).toLowerCase()
-const email = accounts[1]
 
 
-for (let i = 0; i < accounts.length; i++) {
-    const element = accounts[i].toLowerCase();
-    console.log(element)
+button.addEventListener('click', function() {
+    let loginAcc = email.value;
+    console.log(loginAcc)
+    for (let i = 0; i < accounts.length; i++) {
+        const element = accounts[i].toLowerCase();
+        console.log(element)
+        if (loginAcc !== element) {
+          main.append(div)
+          div.innerHTML = "Non sei un utente registrato!"
+        } else {
+            main.append(div)
+            div.innerHTML =  "SEI GIA' REGISTRATO!"
+        }   
+    }
 
-    if (email !== element) {
-      main.append(div)
-      div.innerHTML = "Non sei un utente registrato!"
-    } else {
-        main.append(div)
-        div.innerHTML =  "SEI GIA' REGISTRATO!"
-    }   
-}
+});
