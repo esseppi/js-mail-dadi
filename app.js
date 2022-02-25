c2bf6e77d5dde30eb4561a0ab53bccc86bde017c
esseppidefi <97457479+esseppidefi@users.noreply.github.com>
2022-02-25 17:43:51 +0100

    bho

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 const accounts = ["ciao@gmail.com", "william@gmail.com", "yoyo@gmail.com", "basta@gmail.com", "bella@gmail.com"]
 let email = document.getElementById('email');
 let main = document.querySelector('main');
-
 let div = document.createElement('div');
 main.append(div)
 let button = document.querySelector('.mailbut');
 
+let check = false;
 
 button.addEventListener('click', function() {
     let loginAcc = email.value.toLowerCase();
     console.log(loginAcc)
     for (let i = 0; i < accounts.length; i++) {
         const element = accounts[i].toLowerCase();
-        if (loginAcc !== element) {
-            div.innerHTML = "Non sei un utente registrato!";
-        } else {
-            div.innerHTML =  "SEI GIA' REGISTRATO!";  
-        }   
+        if (element == loginAcc) {
+            check = true
+        } 
     }
+if (check = true) {
+    div.append('entra')
+    
+} else {
+    div.append('esci')
+}
 });
+
+
 
 
 
@@ -29,16 +35,16 @@ let result1 = document.querySelector('.r1')
 let result2 = document.querySelector('.r2')
 
 
-tiraDado.addEventListener('click', function() {
-    let risultato1 = Math.round(Math.random() * 5 + 1);
-   result1.innerHTML = risultato1;
-   let risultato2 = Math.round(Math.random() * 5 + 1);
-   result2.innerHTML = risultato2;
-   if (risultato1 == risultato2) {
-    vincitore.innerHTML ='I giocatori hanno pareggiato'
-   } else if (risultato1 > risultato2) {
-    vincitore.innerHTML ='Giocatore 1 vince';
-   } else {
-       vincitore.innerHTML ='Computer vince'
-   }
-});
+// tiraDado.addEventListener('click', function() {
+//     let risultato1 = Math.round(Math.random() * 5 + 1);
+//    result1.innerHTML = risultato1;
+//    let risultato2 = Math.round(Math.random() * 5 + 1);
+//    result2.innerHTML = risultato2;
+//    if (risultato1 == risultato2) {
+//     vincitore.innerHTML ='I giocatori hanno pareggiato'
+//    } else if (risultato1 > risultato2) {
+//     vincitore.innerHTML ='Giocatore 1 vince';
+//    } else {
+//        vincitore.innerHTML ='Computer vince'
+//    }
+// });
