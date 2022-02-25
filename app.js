fccc14d45cb8ca2ae35f39df1d3e4722861a4e53
esseppidefi <97457479+esseppidefi@users.noreply.github.com>
2022-02-25 18:18:08 +0100

    GRAZIE MAURO

--- app.js
+++ app.js
@@ -8,17 +8,16 @@ let button = document.querySelector('.mailbut');
 button.addEventListener('click', function() {
     let check = false;
     let loginAcc = email.value.toLowerCase();
-    console.log(loginAcc)
     for (let i = 0; i < accounts.length; i++) {
-        const element = accounts[i].toLowerCase();
+        let element = accounts[i].toLowerCase();
         if (element == loginAcc) {
             check = true;
         } 
     }
-    if (check = true) {
-        div.append('entra');  
+    if (check == true) {
+        div.innerHTML = 'Sei registrato';  
     } else {
-        div.append('esci');
+        div.innerHTML = 'Non sei registrato';
     }
 });
 
@@ -33,16 +32,16 @@ let result1 = document.querySelector('.r1')
 let result2 = document.querySelector('.r2')
 
 
-// tiraDado.addEventListener('click', function() {
-//     let risultato1 = Math.round(Math.random() * 5 + 1);
-//    result1.innerHTML = risultato1;
-//    let risultato2 = Math.round(Math.random() * 5 + 1);
-//    result2.innerHTML = risultato2;
-//    if (risultato1 == risultato2) {
-//     vincitore.innerHTML ='I giocatori hanno pareggiato'
-//    } else if (risultato1 > risultato2) {
-//     vincitore.innerHTML ='Giocatore 1 vince';
-//    } else {
-//        vincitore.innerHTML ='Computer vince'
-//    }
-// });
+tiraDado.addEventListener('click', function() {
+    let risultato1 = Math.round(Math.random() * 5 + 1);
+   result1.innerHTML = risultato1;
+   let risultato2 = Math.round(Math.random() * 5 + 1);
+   result2.innerHTML = risultato2;
+   if (risultato1 == risultato2) {
+    vincitore.innerHTML ='I giocatori hanno pareggiato'
+   } else if (risultato1 > risultato2) {
+    vincitore.innerHTML ='Giocatore 1 vince';
+   } else {
+       vincitore.innerHTML ='Computer vince'
+   }
+});
